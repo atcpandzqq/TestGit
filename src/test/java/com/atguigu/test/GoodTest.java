d9aa9956673d22a433b784bc4d5032e5ae551926
zhaoqianqiao <15229408779@163.com>
2021-05-09 11:36:41 +0800

    '添加list'

--- src/test/java/com/atguigu/test/GoodTest.java
+++ src/test/java/com/atguigu/test/GoodTest.java
@@ -1,5 +1,28 @@
 package com.atguigu.test;
 
-public class GoodTest {
+import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class GoodTest {
+    public static void main(String[] args) {
+        int[] lista={1,3,4,3,2,2,2};
+        List<int[]> strList= Arrays.asList(lista);
+        List<Integer> Arrist=new ArrayList<Integer>();
+        Arrist.add(1);
+         Arrist.add(2);
+        Arrist.add(2);
+        Arrist.add(2); Arrist.add(2);
+        Arrist.add(2);
+        System.out.println(Arrist.size());
+        for (int i =  Arrist.size()-1; i >=0; i--) {
+
+            System.out.println(Arrist.get(i));
+            Arrist.remove(Arrist.get(i));
+
+        }
+
+        System.out.println("你好欢迎来到java的世界");
+    }
 }
